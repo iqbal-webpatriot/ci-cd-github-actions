0721c7a802f8a30db2b2a07ee7eea0c9a6fcdc3b
iqbal.webpatriot <iqbal.webpatriot@gmail.com>
2023-08-10 19:11:06 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Chane name 
+         Chane name again 
         </p>
         <a
           className="App-link"
